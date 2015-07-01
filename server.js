b0cde31485e036303a190d5a74272ae20cf77d5f
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-07-01 12:11:02 +0200

    added project and door-project relationship

--- server.js
+++ server.js
@@ -1,33 +1,96 @@
 // given: a Revit door element UniqeId;
 // it must obviously be unique in the database.
 
-var unique_id
+var door_unique_id
   = '60f91daf-3dd7-4283-a86d-24137b73f3da-0001fd0b';
+
+// Document.WorksharingCentralGUID property, if defined,
+// or Document.ProjectInformation.UniqueId.
+
+var project_guid
 
 var mongoose = require( 'mongoose' );
 
-mongoose.connect( 'mongodb://localhost/firerating ');
+mongoose.connect( 'mongodb://localhost/firerating' );
 
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
 var RvtUniqueId = String;
 
-var Door = new Schema(
+// identify Revit project by machine name and full path,
+// System.Environment.MachineName + Document.PathName;
+// this only works for saved non-workshared documents;
+// no idea how to handle that... use automatic
+// ObjectId instead, to handle both types of projects.
+
+var projectSchema = new Schema(
+  { //_id               : RvtProjectPath
+    computername        : String // .NET System.Environment.MachineName
+    , path              : String // Document.PathName
+    , centralserverpath : String // Document.GetWorksharingCentralModelPath().CentralServerPath
+    , title             : String // Document.Title
+    , numberofsaves     : Number // DocumentVersion.NumberOfSaves
+    , versionguid       : RvtUniqueId // DocumentVersion.VersionGUID
+    , projectinfo_uid   : RvtUniqueId } // ProjectInfo.UniqueId
+  //{ _id: false } // suppress automatic generation
+);
+
+var ProjectModel = mongoose.model( 'Project', projectSchema );
+
+var computer_name = 'JEREMYTAMMIB1D2';
+var project_path = 'C:/Program Files/Autodesk/Revit 2016/Samples/rac_basic_sample_project.rvt';
+
+// compress? use MD5? SHA-2! https://en.wikipedia.org/wiki/SHA-2
+// in .NET: System.Security.Cryptography.SHA256Managed
+// but then we might as well use the default mongo objectid...
+
+var projectInstance = new ProjectModel();
+
+projectInstance.computername = 'JEREMYTAMMIB1D2';
+projectInstance.path = 'C:/Program Files/Autodesk/Revit 2016/Samples/rac_basic_sample_project.rvt';
+projectInstance.centralserverpath = '';
+projectInstance.title = 'rac_basic_sample_project.rvt';
+projectInstance.numberofsaves = 271;
+projectInstance.versionguid = 'f498e8b1-7311-4409-a669-2fd290356bb4';
+projectInstance.projectinfo_uid = '8764c510-57b7-44c3-bddf-266d86c26380-0000c160';
+
+var doorSchema = new Schema(
   { _id          : RvtUniqueId // suppress automatic generation
+    , project_id : ObjectId
     , level      : String
     , tag        : String
     , firerating : Number },
   { _id: false } // suppress automatic generation
 );
 
-var DoorModel = mongoose.model( 'Door', Door );
+var DoorModel = mongoose.model( 'Door', doorSchema );
 
-var instance = new DoorModel();
-instance._id = unique_id; // new RvtUniqueId(unique_id);
-instance.level = 'Level 1';
-instance.tag = 'Tag 1';
-instance.firerating = 123.45;
+projectInstance.save(function (err) {
+  console.log( 'save project returned err = ' + err );
+  if(!err) {
+    var pid = projectInstance._id;
+    console.log( 'project_id = ' + pid );
 
-instance.save(function (err) {
-  console.log( 'save returned err = ' + err );
+    var instance = new DoorModel();
+    instance._id = door_unique_id;
+    instance.project_id = pid;
+    instance.level = 'Level 1';
+    instance.tag = 'Tag 1';
+    instance.firerating = 123.45;
+    instance.save(function (err) {
+      console.log( 'save instance returned err = ' + err );
+      if(!err) {
+        var instance2 = new DoorModel();
+        instance2._id = door_unique_id + '2';
+        instance2.project_id = pid;
+        instance2.level = 'Level 2';
+        instance2.tag = 'Tag 2';
+        instance2.firerating = 678.9;
+        instance2.save(function (err) {
+          console.log( 'save instance2 returned err = ' + err );
+        });
+      }
+    });
+  }
 });
