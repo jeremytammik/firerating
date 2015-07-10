edaf3776bc06c5949d6ae51e9f3011192a11e23a
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-07-10 12:51:35 +0200

    removed obsolete revit project document management code

--- routes.js
+++ routes.js
@@ -1,28 +1,9 @@
 module.exports = function(app) {
-
-  //var projects = require('./controller/projects_v1');
-  //app.get('/api/v1/projects', projects.findAll);
-  //app.get('/api/v1/projects/:id', projects.findById);
-  //app.post('/api/v1/projects', projects.add);
-  //app.put('/api/v1/projects/:id', projects.update);
-  //app.delete('/api/v1/projects/:id', projects.delete);
-
-  // Using the ProjectInformation UniqueId is
-  // utterly unreliable, we can stop that right
-  // away. Use computer name and full project
-  // path instead for the time being.
-  // Furthermore, this query can be achieved using
-  // a query string, e.g. "projects?projectinfo_uid=..."
-  //app.get('/api/v1/projects/uid/:uid', projects.findByUniqueId);
-  //app.get('/api/v1/projects/pcnamepath/:s', projects.findByComputerNamePath);
-  //app.get('/api/v1/projects/jid/:jid', projects.findByJid);
-
   var doors = require('./controller/doors_v1');
   app.get('/api/v1/doors', doors.findAll);
   app.get('/api/v1/doors/:id', doors.findById);
   app.post('/api/v1/doors', doors.add);
   app.put('/api/v1/doors/:id', doors.update);
   app.delete('/api/v1/doors/:id', doors.delete);
-
   app.get('/api/v1/doors/project/:pid', doors.findAllForProject);
 }
--- server.js
+++ server.js
@@ -6,50 +6,6 @@
 //
 // Copyright 2015 by Jeremy Tammik, Autodesk Inc.
 
-//// Data model
-//
-//// given: a Revit door element UniqeId;
-//// it must obviously be unique in the database.
-//
-//var door_unique_id
-//  = '60f91daf-3dd7-4283-a86d-24137b73f3da-0001fd0b';
-//
-//var Schema = mongoose.Schema,
-//    ObjectId = Schema.ObjectId;
-//
-//var RvtUniqueId = String;
-//
-//var projectInstance = new ProjectModel();
-//
-//projectInstance.computername = 'JEREMYTAMMIB1D2';
-//projectInstance.path = 'C:/Program Files/Autodesk/Revit 2016/Samples/rac_basic_sample_project.rvt';
-//projectInstance.centralserverpath = '';
-//projectInstance.title = 'rac_basic_sample_project.rvt';
-//projectInstance.numberofsaves = 271;
-//projectInstance.versionguid = 'f498e8b1-7311-4409-a669-2fd290356bb4';
-//projectInstance.projectinfo_uid = '8764c510-57b7-44c3-bddf-266d86c26380-0000c160';
-//
-//projectInstance.save(function (err) {
-//  console.log( 'save project returned err = ' + err );
-//  if(!err) {
-//    var pid = projectInstance._id;
-//    console.log( 'project_id = ' + pid );
-//
-//    for( var i = 0; i < 10; ++i ) {
-//      var inst = new DoorModel();
-//      var s = i.toString();
-//      inst._id = door_unique_id + s;
-//      inst.project_id = pid;
-//      inst.level = 'Level ' + s;
-//      inst.tag = 'Tag ' + s;
-//      inst.firerating = 123.45 * (i + 0.1);
-//      inst.save(function (err) {
-//        console.log( 'save instance returned err = ' + err );
-//      });
-//    }
-//  }
-//});
-
 // Web server
 
 var express = require('express');
@@ -70,15 +26,10 @@ db.on( 'error', function () {
 
 var app = express();
 
-//app.configure( function(){
-//  app.use(express.bodyParser());
-//});
-
 var bodyParser = require( 'body-parser' );
 app.use( bodyParser.json({ limit: '1mb' }) );
 app.use( bodyParser.urlencoded({ extended: true, limit: '1mb' }) );
 
-//require( './model/project' );
 require( './model/door' );
 require( './routes' )( app );
 
