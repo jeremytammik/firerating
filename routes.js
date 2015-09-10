9b9d57ae9e55e01a24f5ee2c248813be83687327
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-09-10 08:32:16 +0200

    implemented update2 so that PUT can be used as POST to both update and create

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ module.exports = function(app) {
   app.get('/api/v1/doors', doors.findAll);
   app.get('/api/v1/doors/:id', doors.findById);
   app.post('/api/v1/doors', doors.add);
-  app.put('/api/v1/doors/:id', doors.update);
+  //app.put('/api/v1/doors/:id', doors.update); // this one does not allow me to PUT a new instance only update existing
+  app.put('/api/v1/doors/:id', doors.update2); // works more like POST + PUT, cf. http://stackoverflow.com/questions/630453/put-vs-post-in-rest
   app.delete('/api/v1/doors/:id', doors.delete);
   app.get('/api/v1/doors/project/:pid', doors.findAllForProject);
 }
