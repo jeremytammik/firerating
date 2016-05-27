17c96beabd483e57f28bee2024f35272fb5e3974
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-05-27 12:07:54 +0200

    reverted

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-module.exports = function(app, io) {
+module.exports = function(app) {
   var DoorService = require('./controller/doors_v1');
   app.get('/api/v1/doors', DoorService.findAll);
   app.get('/api/v1/doors/:id', DoorService.findById);
